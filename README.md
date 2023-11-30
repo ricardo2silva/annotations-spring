e62bef6c58e35c825f94edcc66d46521cf4dc5fd
ricardo2silva <ricardojonasdasilva60@gmail.com>
2023-11-30 00:08:29 -0300

    @Autowired

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ## @Component
 
 ### Quando utilizamos a anotaçao @Component, estamos dizendo ao spring que aquela classe anotada, deverá ser gerenciada por ele, criando  uma instancia e gerenciando a, de forma que ela possa ser injetada em outras classes.
-### Por algum motivo quando colocamos o @Component em uma classe de controller, ele nao funciona , ao tentar bater em um endpoint ele retorna 404 NOT FOUND
+### Por algum motivo quando colocamos o @Component em uma classe de controller, ele nao funciona, ao tentar fazer uma requisição em um endpoint ele retorna 404 NOT FOUND
 
+## @AutoWired
+
+### anotação usada normalmente para fazer injeção de dependencia sem a necessidade do construtor.
--- src/main/java/com/ricardo/projetobase/controllers/ProdutoController.java
+++ src/main/java/com/ricardo/projetobase/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package com.ricardo.projetobase.controllers;
 import com.ricardo.projetobase.models.Produto;
 import com.ricardo.projetobase.models.dtos.ProdutoDTO;
 import com.ricardo.projetobase.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +13,8 @@ import java.util.List;
 @Component
 @RequestMapping("/produtos")
 public class ProdutoController {
-
-    private final ProdutoService produtoService;
-
-    public ProdutoController(ProdutoService produtoService) {
-        this.produtoService = produtoService;
-    }
+    @Autowired
+    private  ProdutoService produtoService;
 
     @PostMapping("/salvarProduto")
     public ResponseEntity<Produto> salvarProduto(@RequestBody ProdutoDTO produtoDTO) {
